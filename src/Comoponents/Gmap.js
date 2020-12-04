b401c20fdbe123accfed2ef1cf70b403b210fb49
dasri7842 <dasarisrinivas7842@gmail.com>
2020-12-04 07:13:35 +0530

    Small changes

--- src/Comoponents/Gmap.js
+++ src/Comoponents/Gmap.js
@@ -2,11 +2,13 @@ import GoogleMapReact from 'google-map-react';
 import {useState } from 'react';
 import Datapop from './Datapop'
 import Fire from './Fire'
+import { v4 as uuidv4 } from 'uuid';
 const Gmap = ({data}) => {
    const [datapop, setdatapop] = useState(null);
    const markers = data.map(ev => {
       if(ev.categories[0].id !== 12) 
          return <Fire
+                  key={uuidv4()}
                   lat={ev.geometries[0].coordinates[1]}
                   lng={ev.geometries[0].coordinates[0]} 
                   datapop={()=>{
